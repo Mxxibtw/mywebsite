15b7c00f424c991008316fbe5c36c7d857ba0e51
Maxi <50955764+Mxxibtw@users.noreply.github.com>
2022-02-22 15:26:10 +0100

    add meta tags, robots.txt & terminal structure

---
+++ com/app.js
@@ -0,0 +1,6 @@
+console.log("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+console.log("");
+console.log("If you are interested in code, visit my GitHub repositories.");
+console.log("https://github.com/Mxxibtw/");
+console.log("");
+console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
